bec3cff8c39947569821ddb986625a144c559c32
Raphael D. Pinheiro <raphaklaus@gmail.com>
2016-08-20 22:37:17 -0300

    Update LengthError.js

--- src/Errors/LengthError.js
+++ src/Errors/LengthError.js
@@ -4,6 +4,6 @@ module.exports = class LengthError extends Error {
   constructor(message) {
     super(message);
     this.message = message;
-    this.name = 'UndefinedError';
+    this.name = 'LengthError';
   }
 };
